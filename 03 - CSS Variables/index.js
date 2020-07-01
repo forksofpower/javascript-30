b6b86de70b806e014b613bb465dbbf0ca4f42c07
Patrick Jones <patrickjones.pmj@gmail.com>
2020-07-01 02:43:57 -0500

    add event listeners to all inputs; change css variables on change and mousemove

---
+++ 03 - CSS Variables/index.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll('input')
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+// setup event listeners
+inputs.forEach(input => {
+    input.addEventListener('change', handleUpdate)
+    input.addEventListener('mousemove', handleUpdate)
+})
+
