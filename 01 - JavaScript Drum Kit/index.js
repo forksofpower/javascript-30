739535ae515a3c4176f7d371d6cf2bb4fcfb7182
Patrick Jones <patrickjones.pmj@gmail.com>
2020-07-01 00:57:07 -0500

    split js to file; add transitionend eventlistener to remove styles

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,34 @@
+const keys = document.querySelectorAll('.key')
+
+keys.forEach(key => {
+    // listen for the end of each transition
+    key.addEventListener('transitionend', removeTransition)
+})
+
+function removeTransition(e) {
+    // remove `playing` class if transform is finished
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove('playing')
+}
+
+function getPad(keycode) {
+    return document.querySelector(`.key[data-key='${keycode}']`)
+}
+
+function getAudio(keycode) {
+    return document.querySelector(`audio[data-key='${keycode}']`)
+}
+
+function playSound(e) {
+    // check if its a drum pad key
+    let pad = getPad(e.keyCode)
+    if (!pad) return;
+    // play audio
+    let audio = getAudio(e.keyCode)
+    audio.currentTime = 0
+    audio.play()
+    // apply `playing` class    
+    pad.classList.add('playing')
+}
+
+window.addEventListener('keydown', playSound)
